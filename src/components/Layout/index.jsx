dae91513423de5374a0f5619928e526683d654e1
Jason Hoang <jasonhoang6201@gmail.com>
2022-07-31 22:30:35 +0700

    edit menu

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,27 +2,36 @@ import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Layout as LayoutAntd, Menu } from "antd";
 import "./layout.css";
 
-import React, { useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import logo from "src/assets/images/logo.png";
 import useRouting from "src/hooks/UseRouting";
 import routers from "src/routers";
 const { Header, Sider, Content } = LayoutAntd;
 
 const Layout = () => {
+  const location = useLocation();
   const { generate } = useRouting();
   const navigate = useNavigate();
   const [collapsed, setCollapsed] = useState(false);
+  const [activeKey, setActiveKey] = useState(0);
+
+  useEffect(() => {
+    const { pathname } = location;
+    const key = routers.findIndex((router) => router.path === pathname);
+    setActiveKey(key);
+  }, [location]);
+
   return (
     <LayoutAntd>
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo h-[64px] flex items-center justify-center">
-          <img src={logo} width={100} height={100} />
+          <img src={logo} width={100} height={100} alt="logo" />
         </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          activeKey={[activeKey]}
           items={routers.map((item, index) => ({
             label: item.name.toUpperCase(),
             icon: item.icon,
